1383d6aeb7d93fd2153c192fe920919de4929917
hmrguez <zealot.algo@gmail.com>
2024-10-06 10:29:44 -0400

    Separating by approved, exceptions and timeouts

--- AutoGrader/Solution.cs
+++ AutoGrader/Solution.cs
@@ -4,6 +4,20 @@ public static class Solution
 {
     public static int Solve(int a)
     {
-        return 5;
+        var random = new Random();
+
+        var inta = random.Next(3);
+        switch (inta)
+        {
+            case 0:
+                return 5;
+            case 1:
+                throw new ArgumentException();
+            case 2:
+                System.Threading.Thread.Sleep(10000);
+                throw new Exception();
+        }
+
+        throw new Exception();
     }
 }
