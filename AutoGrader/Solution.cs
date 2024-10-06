c9a3f6bced8b6f204da00e08b909d4fde259faa6
hmrguez <zealot.algo@gmail.com>
2024-10-06 11:43:23 -0400

    Kind of more framework

--- AutoGrader/Solution.cs
+++ AutoGrader/Solution.cs
@@ -4,20 +4,22 @@ public static class Solution
 {
     public static int Solve(int a)
     {
-        var random = new Random();
+        return a * Solve(a);
 
-        var inta = random.Next(3);
-        switch (inta)
-        {
-            case 0:
-                return 5;
-            case 1:
-                throw new ArgumentException();
-            case 2:
-                System.Threading.Thread.Sleep(10000);
-                throw new Exception();
-        }
-
-        throw new Exception();
+        // var random = new Random();
+        //
+        // var inta = random.Next(4);
+        // switch (inta)
+        // {
+        //     case 0:
+        //         return 5;
+        //     case 1:
+        //         throw new ArgumentException();
+        //     case 2:
+        //         System.Threading.Thread.Sleep(100000);
+        //         throw new Exception();
+        // }
+        //
+        // throw new Exception();
     }
 }
