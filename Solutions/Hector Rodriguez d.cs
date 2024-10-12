f1bbede684a0c833bbc277041b9493102e83373e
hmrguez <zealot.algo@gmail.com>
2024-10-12 14:16:41 -0400

    TEsting

---
+++ Solutions/Hector Rodriguez d.cs
@@ -0,0 +1,9 @@
+namespace Exam;
+
+public static class Solution
+{
+    public static int Solve(int a, int b)
+    {
+        return 5 + b;
+    }
+}
---
+++ Solutions/Miguel Sosa d.cs
@@ -0,0 +1,9 @@
+namespace Exam;
+
+public static class Solution
+{
+    public static int Solve(int a, int b)
+    {
+        return a * 2 + b * 2;
+    }
+}
