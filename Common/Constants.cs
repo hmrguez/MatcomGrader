6264703c458d50785a670e605dda522857b1b105
hmrguez <zealot.algo@gmail.com>
2024-10-12 14:23:57 -0400

    Temp

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -2,7 +2,7 @@ namespace Common;
 
 public static class Constants
 {
-    public static readonly string ResultFileName = "result.md";
+    public static readonly string ResultFileName = "results.md";
     public static readonly string[] GlobalHeaders =
         ["Name", "Score", "✅ Correct", "⭕️ Wrong", "‼️ Exceptions", "⏰ Timeouts"];
 
